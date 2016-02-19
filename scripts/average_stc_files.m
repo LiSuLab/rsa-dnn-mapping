f6dfdb297aeafa23a5bc229749f07f77052a6e4e
Cai Wingfield <cw417@cam.ac.uk>
2016-02-19 19:39:29 +0000

    Bugfixes.

--- scripts/average_stc_files.m
+++ scripts/average_stc_files.m
@@ -15,7 +15,7 @@ function average_stc_paths = average_stc_files(map_paths, name_prefix, userOptio
         
         for lag_i = 1:n_lags
             
-            prints('Reading stc file %s...', map_paths(lag_i).(chi));
+            prints('(%d/%d) Reading stc file %s...', lag_i, n_lags, map_paths(lag_i).(chi));
             
             stc_struct = mne_read_stc_file1(map_paths(lag_i).(chi));
             
@@ -28,24 +28,24 @@ function average_stc_paths = average_stc_files(map_paths, name_prefix, userOptio
                 % later.
                 % ones() because we've got at least one datapoint from this
                 % zero-lag model.
-                n_timepoints = size(sum_data, 2);
-                n_contributions = ones(1, n_timepoints);
+                [n_verts, n_timepoints] = size(sum_data);
+                contribution_counts = ones(1, n_timepoints);
             else
                 
                 % Trim the first frame of this data
-                this_data = stc_struct.data(2:end, :);
+                this_data = stc_struct.data(:, lag_i:end);
                 
                 % Record this contribution
-                width_this_data = size(this_data, 1);
-                n_contributions(1:width_this_data) = n_contributions(1:width_this_data) + 1;
+                trimmed_epoch_length = size(this_data, 2);
+                contribution_counts(1:trimmed_epoch_length) = contribution_counts(1:trimmed_epoch_length) + 1;
                 
-                sum_data = sum_data + this_data;
+                sum_data(:, 1:trimmed_epoch_length) = sum_data(:, 1:trimmed_epoch_length) + this_data;
             end
             
         end%for:file_i
         
         % Mean the remaining data, appropriate
-        average_data = sum_data ./ repmat(n_contributions, n_verts, 1);
+        average_data = sum_data ./ repmat(contribution_counts, n_verts, 1);
         
         % Reuse an stc struct with all the right vertices and tstep in it already.
         reusable_stc_struct.data = average_data;
