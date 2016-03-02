0ca76efa591bd351182d8fe5319d00d2f7d81f3a
Cai Wingfield <cw417@cam.ac.uk>
2016-03-02 11:16:52 +0000

    Add MFCC searchlight code.

---
+++ Recipe_cepstral_searchlight.m
@@ -0,0 +1,183 @@
+userOptions = slOptions_cepstral();
+
+
+%% %%%%%%%%%%%%%%%%%%%
+rsa.util.prints('Running toolbox for %s', userOptions.analysisName);
+%%%%%%%%%%%%%%%%%%%%%%
+
+dynamic_model_RDM = rsa.util.directLoad('/imaging/cw04/CSLB/Analysis_Cepstral/models/cepstral_models_win20_C01-C12');
+n_lags = numel(dynamic_model_RDM);
+
+MODEL_TIMESTEP_ms = 10;
+
+
+%% %%%%%%%%%%%%%%%%%%%
+rsa.util.prints( ...
+    'Preparing masks...');
+%%%%%%%%%%%%%%%%%%%%%%
+
+usingMasks = ~isempty(userOptions.maskNames);
+if usingMasks
+    slMasks = rsa.meg.MEGMaskPreparation_source(userOptions);
+    % For this searchlight analysis, we combine all masks into one
+    slMasks = rsa.meg.combineVertexMasks_source(slMasks, 'combined_mask', userOptions);  
+else
+    slMasks = rsa.meg.allBrainMask(userOptions);
+end
+
+
+%% Compute some constats
+nSubjects = numel(userOptions.subjectNames);
+adjacencyMatrix = rsa.meg.calculateMeshAdjacency(userOptions.targetResolution, userOptions.sourceSearchlightRadius, userOptions);
+
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+rsa.util.prints( ...
+    'Starting parallel toolbox...');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if userOptions.flush_Queue
+    rsa.par.flushQ();
+end
+
+if userOptions.run_in_parallel
+    p = rsa.par.initialise_CBU_Queue(userOptions);
+end
+
+
+%% %%%%%%%%%%%%%%%%%%
+rsa.util.prints( ...
+    'Loading brain data...');
+%%%%%%%%%%%%%%%%%%%%%
+
+[meshPaths, STCMetadatas] = rsa.meg.MEGDataPreparation_source( ...
+    lexproBetaCorrespondence(), ...
+    userOptions, ...
+    'mask', slMasks);
+
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+rsa.util.prints( ...
+    'Searchlight Brain RDM Calculation...');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[RDMsPaths, slSTCMetadatas, slSpecs] = optimisedMEGSearchlightRDMs_source( ...
+    meshPaths, ...
+    slMasks, ...
+    adjacencyMatrix, ...
+    STCMetadatas, ...
+    userOptions);
+
+
+%% %%%%%
+rsa.util.prints( ...
+    'Averaging searchlight RDMs...');
+%%%%%%%%
+
+averageRDMPaths = averageSlRDMs( ...
+    RDMsPaths, ...
+    slMasks, ...
+    lexproBetaCorrespondence(), ...
+    userOptions);
+
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+rsa.util.prints( ...
+   'Searchlight Brain Model comparison...');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+n_subjects = numel(userOptions.subjectNames);
+   
+% Work on each hemisphere separately
+for chi = 'LR'
+    
+    for subject_i = 1:n_subjects
+        
+        subjectName = userOptions.subjectNames{subject_i};
+
+        for lag_i = 1:n_lags
+
+           this_lag_ms = (lag_i - 1) * MODEL_TIMESTEP_ms;
+
+           rsa.util.prints('%sH: Subj %d/%d. Lag %d/%d...', chi, subject_i, n_subjects, lag_i, n_lags);
+
+           % TODO: fix the use of sl metadatas and sl specs here
+           [mapPaths(subject_i, lag_i).(chi)] = lag_fixed_searchlight_mapping_source( ...
+               chi, ...
+               ...% can't deref .(chi) here as it auto-expands the argument
+               RDMsPaths(subject_i, :), ...
+               sprintf('%s_subj_%s', userOptions.analysisName, subjectName), ...
+               ...% Use the mask for this hemisphere only
+               slMasks([slMasks.chi] == chi), ...
+               dynamic_model_RDM(lag_i), this_lag_ms, ...
+               ...% Apparently we don't deref .(chi) here either
+               STCMetadatas, ...
+               userOptions);
+
+        end
+    end
+end
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%
+rsa.util.prints(...
+    'Averaging STC files...');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+for subject_i = 1:n_subjects
+    
+    subjectName = userOptions.subjectNames{subject_i};
+   
+    averageMapPaths(subject_i) = lag_integrate_stc_files(...
+        mapPaths(subject_i, :), ...
+        sprintf('%s_subj_%s_lagfixed', userOptions.analysisName, subjectName), ...
+        userOptions, ...
+        true);
+
+end
+
+true_paths = average_stc_files( ...
+    averageMapPaths, ...
+    ...% use -1 so it fits into three digits in the name
+    sprintf('%s_subave_lagfixed', userOptions.analysisName), ...
+    false, ...
+    userOptions);
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%
+rsa.util.prints(...
+    'RFX stats...');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+n_perms = 1000;
+parfor perm_i = 1:n_perms
+    rsa.util.prints('RFX perm %d...', perm_i);
+    rfx_paths(perm_i) = average_stc_files( ...
+        averageMapPaths, ...
+        ...% use -1 so it fits into three digits in the name
+        sprintf('%s_rfx_perm%03d', userOptions.analysisName, perm_i-1), ...
+        true, ...
+        userOptions);
+end
+
+
+rsa.util.prints('Computing thresholds...');
+confidence_level = 0.05;
+thresholds = rfx_threshold( ...
+    rfx_paths, ...
+    confidence_level);
+
+rsa.util.prints('<-------------------------------------------------------->');
+rsa.util.prints(' RFX tresholds at confidence level %d are [L: %d | %d :R].', confidence_level, thresholds.L, thresholds.R);
+rsa.util.prints('<-------------------------------------------------------->');
+
+
+%% Send an email
+
+rsa.par.setupInternet();
+rsa.par.setupEmail(userOptions.mailto);
+
+
+%% Stop parallel toolbox
+
+if userOptions.run_in_parallel;
+    delete(p);
+end
---
+++ slOptions_cepstral.m
@@ -0,0 +1,248 @@
+function userOptions = slOptions()
+
+%%%%%%%%%%%%%%%%%%%%%
+%% Project details %%
+%%%%%%%%%%%%%%%%%%%%%
+
+% This name identifies a collection of files which all belong to the same run of a project.
+userOptions.analysisName = 'lexpro-bn-searchlight';
+
+% This is the root directory of the project.
+userOptions.rootPath = '/imaging/cw04/CSLB/Analysis_Cepstral/CWD_win25_lateral_5121';
+
+% The path leading to where the scans are stored (not including subject-specific identifiers).
+% "[[subjectName]]" should be used as a placeholder to denote an entry in userOptions.subjectNames
+% "[[betaIdentifier]]" should be used as a placeholder to denote an output of betaCorrespondence.m if SPM is not being used; or an arbitrary filename if SPM is being used.
+userOptions.betaPath = '/imaging/at03/NKG_Code_output/Version4_2/LexproMEG/3-single-trial-source-data/vert10242-smooth5-nodepth-eliFM-snr1-signed/[[betaIdentifier]]';
+
+%%%%%%%%%%%%%%%%%%%
+%% Email Options %%
+%%%%%%%%%%%%%%%%%%%
+
+% Set to true to be informed when a script finishes.
+userOptions.recieveEmail = true;
+% Put your address to make 
+userOptions.mailto = 'cw417@cam.ac.uk';
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Parallel Computing toolbox %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% To run parallel locally set run_in_parallel *and*
+% run_in_parallel_in_cluster to false. 
+% To run on CBU adaptive queue set run_in_parallel_in_cluster to true. 
+% Do NOT set this true for fixed effect analysis (searchlight and
+% permutation.
+userOptions.run_in_parallel = true;
+userOptions.run_in_parallel_in_cluster= true;
+% Sometimes the performance will drop if there are large number of tiny
+% jobs due to the communication and setting-up overhead.
+userOptions.jobSize = 1;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Adaptive Computing Cluster Queueing OPTIONS %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Set true to delete jobs from the queue otherwise set to false.
+% If you do not want to delete all jobs but only sepecific one do not set
+% this variable to true.
+userOptions.flush_Queue = true; 
+% Used only when using CBU cluster.
+% i.e. when run_in_parallel_in_cluster = true;
+userOptions.wallTime = '24:00:00';
+% Cluster machines requested.
+userOptions.nodesReq = 16;
+% Processors requested per processor machine.
+userOptions.proPNode = 1;
+% The product of nodesReq and proPNode should be greater or equal to the
+% number of workers requested.
+userOptions.nWorkers = 16;
+% In gigabytes, to be distributed amongst all nodes.
+userOptions.memReq = 1440;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Modality-agnostic analysis options %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% The path to a stereotypical mask data file is stored (not including subject-specific identifiers).
+% "[[subjectName]]" should be used as a placeholder to denote an entry in userOptions.subjectNames
+% "[[maskName]]" should be used as a placeholder to denote an entry in userOptions.maskNames
+%userOptions.maskPath = '/imaging/fj01/cw04/New_Labels/[[maskName]]';
+userOptions.maskPath = '/imaging/ef02/lexpro/subject/average/label/[[maskName]]';
+
+% The list of mask filenames (minus .hdr extension) to be used.
+% For MEG, names should be in pairs, such as maskName-lh,
+% maskName-rh.
+% Leave empty to do whole-brain analysis.
+%
+% For MEG sensor-level analysis, only the use of a single mask is
+% supported.
+userOptions.maskNames = { ...
+    ...%'STG_STS_HG-lh', 'STG_STS_HG-rh', ...
+    'lateral-lh', 'lateral-rh',...
+};
+
+% The type of pattern to look at.
+% Options are:
+%     Correlate over space ('spatial')
+%     Correlate over time ('temporal')
+%     Correlate over space and time ('spatiotemporal')
+% For fMRI, the available options are 'spatial'.
+% For MEG, the all options are available.
+userOptions.searchlightPatterns = 'spatiotemporal';
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+%% EXPERIMENTAL SETUP %%
+%%%%%%%%%%%%%%%%%%%%%%%%
+
+% The list of subjects to be included in the study.
+userOptions.subjectNames = { ...
+    ...'meg08_0319', ... % andy excluded this from preprocessing for noise
+    'meg08_0320', ... 
+    'meg08_0323', ...
+    'meg08_0324', ...	
+    'meg08_0327', ...
+    'meg08_0348', ...		
+    'meg08_0350', ... 
+    'meg08_0363', ...
+    'meg08_0366' ...
+    'meg08_0371', ...
+    'meg08_0372', ...    
+    'meg08_0377' , ...
+    'meg08_0380', ...	
+    'meg08_0397', ...
+    'meg08_0400', ...		
+    'meg08_0401', ...
+    'meg08_0402' ...
+};% eg CBUXXXXX
+
+% The default colour label for RDMs corresponding to RoI masks (as opposed to models).
+userOptions.RoIColor = [0 0 1];
+userOptions.ModelColor = [0 1 0];
+
+%% %% %% %% %%
+%%  MEG  %% Use these next four options if you're working in MEG:
+%% %% %% %% %%
+
+% The average surface files
+userOptions.averageSurfaceFiles.L = '/imaging/ef02/lexpro/subject/average/surf/lh.inflated';
+userOptions.averageSurfaceFiles.R = '/imaging/ef02/lexpro/subject/average/surf/rh.inflated';
+
+% The width of the sliding window (ms)
+userOptions.temporalSearchlightWidth = 25;
+
+% The timestep for sliding window (ms)
+userOptions.temporalSearchlightTimestep = 10;
+
+% The overall window of interest for searchlight (ms)
+userOptions.temporalSearchlightLimits = [0, 370];
+
+% TODO: THis should n't be needed, bu tit is.
+userOptions.maskTimeWindows = {[0,370], [0,370]};
+
+% Temporal downsampling
+% E.g., a value of 10 here means only taking each 10th point in time.
+userOptions.temporalDownsampleRate = 1;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% MEG SOURCE-LEVEL ANALYSIS %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% The radius of the source-space searchlight (in mm)
+userOptions.sourceSearchlightRadius = 20;
+
+% Spatial downsampling.
+% Set the target number of vertices per hemisphere.
+userOptions.targetResolution = 5121;%10242;
+
+% TODO: Explain this
+% 5mm is the smallest distance between two adjacent vertex in 10242 resolution.
+% 10mm is the smallest distance between two adjacent vertex in 2562 resolution.
+userOptions.minDist = 5; %mm
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% First-order analysis %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Text lables which may be attached to the conditions for MDS plots.
+userOptions.conditionLabels = {};
+
+% What colours should be given to the conditions?
+userOptions.conditionColours = [repmat([1 0 0], 48,1); repmat([0 0 1], 44,1)];
+
+% Which distance measure to use when calculating first-order RDMs.
+userOptions.distance = 'Correlation';
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Second-order analysis %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Which similarity-measure is used for the second-order comparison.
+userOptions.RDMCorrelationType = 'Spearman';
+
+% How many permutations should be used to test the significance of the
+% fits?  (10,000 highly recommended.)
+userOptions.significanceTestPermutations = 10000;
+
+% Bootstrap options
+userOptions.nResamplings = 1000;
+userOptions.resampleSubjects = true;
+userOptions.resampleConditions = false;
+
+userOptions.fisher = true;
+
+% Group statistics options: random effect ('RFX') or fixed effect ('FFX')
+% TODO: requires a lot more explanation
+userOptions.groupStats = 'RFX';
+
+% Clustering analysis: primary cluster-forming threshold in terms of the
+% top x% for all vertexes across space and time.
+% It means that primary threshold is set to p<x ,one tailed for positive
+% clusters. This doesnot need to be adjusted according to the degree of
+% freedom of data. If the requirement is to select top ONLY x% of all data, set
+% this value to nan.
+% If not using tmaps for RFX, this value only will define the primary threshold.
+% DoF does not help compute threshold for that case.
+userOptions.primaryThreshold = 0.05;
+
+% Should RDMs' entries be rank transformed into [0,1] before they're displayed?
+userOptions.rankTransform = true;
+
+%%%%%%%%%%%%%%%%%%%%
+%% Figure options %%
+%%%%%%%%%%%%%%%%%%%%
+
+% Should rubber bands be shown on the MDS plot?
+userOptions.rubberbands = false;
+
+% What criterion shoud be minimised in MDS display?
+userOptions.criterion = 'metricstress';
+
+% What is the colourscheme for the RDMs?
+userOptions.colourScheme = hot;
+
+% How should figures be outputted?
+userOptions.displayFigures = true;
+userOptions.saveFiguresPDF = false;
+userOptions.saveFiguresFig = false;
+userOptions.saveFiguresPS = false;
+% Which dots per inch resolution do we output?
+userOptions.dpi = 300;
+% Remove whitespace from PDF/PS files?
+% Bad if you just want to print the figures since they'll
+% no longer be A4 size, good if you want to put the figure
+% in a manuscript or presentation.
+userOptions.tightInset = false;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Interaction options %%
+%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% TODO: Need a better solution to this. Enum?
+% This can be used to force a reply to each propt about overwriting files.
+% It can be useful when running unsupervised, or in parallel, where the
+% prompt may not even be seen.
+% Set to 'a', 'r', 's'; or use '' to not force a reply.
+userOptions.forcePromptReply = 's';
+
+end%function
