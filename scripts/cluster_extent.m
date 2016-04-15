d41bbfeedd1761a6aa2170829c89ac0e9e15e018
Cai Wingfield <cw417@cam.ac.uk>
2016-04-15 10:02:39 +0100

    Arbitrary cluster stats.

---
+++ scripts/cluster_extent.m
@@ -0,0 +1,19 @@
+% Given some spatiotemporal maps and some identically-sized cluster labels,
+% this will return a cluster-label-indexed vector of cluster exceedence
+% masses.
+function cluster_stats = cluster_extent(labelled_spatiotemporal_clusters, group_maps, vertex_level_threshold)
+    
+    cluster_ids = unique(labelled_spatiotemporal_clusters);
+    % The cluster whose id is zero is not a cluster at all, so we delete it
+    % here. It's the background!
+    cluster_ids = cluster_ids(cluster_ids > 0);
+    
+    % preallocate
+    cluster_stats = nan(size(cluster_ids));
+    
+    for cluster_i = cluster_ids'
+        % cluster exceedence mass
+        this_cluster_location = (labelled_spatiotemporal_clusters == cluster_i);
+        cluster_stats(cluster_i) = numel(find(this_cluster_location));
+    end
+end
---
+++ scripts/cluster_peak.m
@@ -0,0 +1,20 @@
+% Given some spatiotemporal maps and some identically-sized cluster labels,
+% this will return a cluster-label-indexed vector of cluster exceedence
+% masses.
+function cluster_stats = cluster_peak(labelled_spatiotemporal_clusters, group_maps, vertex_level_threshold)
+    
+    cluster_ids = unique(labelled_spatiotemporal_clusters);
+    % The cluster whose id is zero is not a cluster at all, so we delete it
+    % here. It's the background!
+    cluster_ids = cluster_ids(cluster_ids > 0);
+    
+    % preallocate
+    cluster_stats = nan(size(cluster_ids));
+    
+    for cluster_i = cluster_ids'
+        % cluster exceedence mass
+        this_cluster_location = (labelled_spatiotemporal_clusters == cluster_i);
+        cluster_values = group_maps(this_cluster_location);
+        cluster_stats(cluster_i) = max(cluster_values(:));
+    end
+end
