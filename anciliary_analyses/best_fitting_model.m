131952254939de398db32faa41d5b62dce3da82d
Cai Wingfield <cw417@cam.ac.uk>
2016-07-16 18:33:51 +0100

    bugfixes

--- anciliary_analyses/best_fitting_model.m
+++ anciliary_analyses/best_fitting_model.m
@@ -3,7 +3,6 @@ function [  ] = best_fitting_model()
     import rsa.*
     import rsa.meg.*
 
-    % TODO: put correct model names in here
     models_to_chose_from = { ...
         'FBK', ...
         'L2', ...
@@ -13,25 +12,25 @@ function [  ] = best_fitting_model()
         'triphone', ...
         'feature'};
 
-    maps_base_path = '/imaging/cw04/CSLB/Lexpro/Analysis_NN_mapping/CWD_win25_language_10242/';
+    maps_base_path = '/imaging/cw04/CSLB/Lexpro/Analysis_DNN/CWD_win25_language_10242/';
     all_vals_template_template = fullfile(maps_base_path, 'Maps_%s/lexpro-bn-sl_group_t-map_tfce-%sh.stc');
     
     for chi = 'LR'
-        for model_i = 1:nume(models_to_chose_from)
+        for model_i = 1:numel(models_to_chose_from)
            model = models_to_chose_from{model_i};
            
            %% Load model and insert into stack
            
            this_model_path = sprintf(all_vals_template_template, model, lower(chi));
            
-           stc_metadata = mne_read_stc_file_1(this_model_path);
+           stc_metadata = mne_read_stc_file1(this_model_path);
            
-           if model == 1
+           if model_i == 1
                [n_vertices, n_timepoints] = size(stc_metadata.data);
                all_model_stack = zeros(n_vertices, n_timepoints, numel(models_to_chose_from));
            end
            
-           all_model_stack(:, :, model_i) = stc_metadata;
+           all_model_stack(:, :, model_i) = stc_metadata.data;
            
         end
        
@@ -40,14 +39,13 @@ function [  ] = best_fitting_model()
         % Best models at each vertex,timepoint
         [max_vals, max_val_is] = max(all_model_stack, [], 3);
         
-        % Best model over all time at each vertex
-        [max_vals_overall, max_val_overall_ts] = max(max_vals, [], 2);
-        max_val_overall_is = max_val_is(:, max_val_overall_ts);
+        % Best model over time-averaged values at each vertex
+        [max_vals_overall, max_val_overall_is] = max(squeeze(mean(all_model_stack, 2)), [], 2);
 
         %% Write out maps
         
-        max_vals_path = sprintf(fullfile(maps_base_path, 'Summary_maps', 'best_model_each_timepoint_%sh.stc'), lower(chi));
-        max_vals_overall_path = sprintf(fullfile(maps_base_path, 'Summary_maps', 'best_model_overall_%sh.stc'), lower(chi));
+        max_vals_path = sprintf(fullfile(maps_base_path, 'Summary_maps', 'best_model_each_timepoint-%sh.stc'), lower(chi));
+        max_vals_overall_path = sprintf(fullfile(maps_base_path, 'Summary_maps', 'best_model_overall-%sh.stc'), lower(chi));
         
         write_stc_file(stc_metadata, max_val_is, max_vals_path);
         write_stc_snapshot(stc_metadata, max_val_overall_is, max_vals_overall_path);
